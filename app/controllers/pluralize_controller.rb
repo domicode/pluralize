3ec3091111f978f2d47040ee4cbf2f66c09c199d
domicode <domi@domidesign.ch>
2014-04-16 16:14:27 -0400

    changed functionality of controller

--- app/controllers/pluralize_controller.rb
+++ app/controllers/pluralize_controller.rb
@@ -4,7 +4,11 @@ class PluralizeController < ApplicationController
   end
 
   def create
-    @word = params[:word].pluralize
-    render :show
+    if params[:word].empty?
+      redirect_to pluralize_show_path
+    else
+      @word = params[:word].pluralize
+      render :show
+    end
   end
 end
