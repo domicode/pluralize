53d7c6467653f10fa5cb46ae03067e4d4440a225
domicode <domi@domidesign.ch>
2014-04-17 09:49:58 -0400

    added analytics and social media

--- app/controllers/pluralize_controller.rb
+++ app/controllers/pluralize_controller.rb
@@ -6,6 +6,10 @@ class PluralizeController < ApplicationController
   def create
     if params[:word].empty?
       redirect_to pluralize_show_path
+    elsif params[:word] == params[:word].pluralize
+      @word = params[:word]
+      flash.now[:info] = "Your word didn't change because either the plural is the same as singular or you already entered the plural form."
+      render :show
     else
       @word = params[:word].pluralize
       render :show
